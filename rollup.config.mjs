5ca0ec3c6a8a1b2b9e922a4a15f58059f1cb6df2
neocarto <nicolas.lambert@cnrs.fr>
2026-03-20 10:56:59 +0100

    fix bugs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,32 @@
-// import de nos plugins
 import commonjs from "@rollup/plugin-commonjs";
 import noderesolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
+import { babel } from "@rollup/plugin-babel";
 import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.js",
-  output: {
-    format: "umd",
-    file: "dist/index.min.js",
-    name: "geogrid",
-  },
+  output: [
+    {
+      file: "dist/index.min.js",
+      format: "umd",
+      name: "geogrid",
+      inlineDynamicImports: true,
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    commonjs(), // prise en charge de require
-    noderesolve(), // prise en charge des modules depuis node_modules
-    babel({ babelHelpers: "bundled" }), // transpilation
-    terser(), // minification
+    noderesolve(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      presets: [["@babel/preset-env", { targets: ">0.25%, not dead" }]],
+    }),
+    terser(),
   ],
 };
